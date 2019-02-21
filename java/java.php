eccc44df28803c72760490fbdc6abea6cf5190db
victorybringer <843517609@qq.com>
2019-02-21 22:35:08 +0800

    full

---
+++ java/java.php
@@ -0,0 +1,52 @@
+
+<?php 
+
+$s=file_get_contents("php://input");
+
+$jsondata=json_decode($s);
+$code=$jsondata->code; 
+$partname=$jsondata->name; 
+$fullname=$partname.".java"; 
+$myfile = fopen($fullname, "w");
+
+fwrite($myfile, $code);
+
+exec("javac  -encoding utf-8  ".$fullname."  2>&1",$out);
+
+exec("rm ".$fullname);
+
+if(count($out)!=0){
+for($a=0;$a<count($out);$a++){
+echo $out[$a]."\n";
+
+}
+
+}
+
+else{
+exec("java ".$partname."  2>&1",$out);
+
+for($a=0;$a<count($out);$a++){
+echo $out[$a]."\n";
+
+}
+exec("rm ".$partname.".class");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?> 
+
